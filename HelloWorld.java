7f943d149f7d42a7bd7995361745c9a88aed1be4
Enjoli Rogers <mable@MacBook-Pro.hsd1.or.comcast.net>
2018-11-12 22:53:51 -0800

    create Hello World Class

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Say Hello
+ * @author Enjoli Rogers
+ * @version 2018.11.12
+ */
+public class HelloWorld
+{
+    /**
+     * Say hello to the world
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+}
